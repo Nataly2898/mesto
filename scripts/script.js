a3face6dd16b8753bd7a80fb72fcf19562e11cb8
Nataly Sagalaeva <garna@yandex.ru>
2022-02-22 22:48:40 +0300

    Popup

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+const popup = document.querySelector('.popup');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const formElement = document.querySelector('.popup__form');
+const popupFieldName = popup.querySelector('.popup__field_name');
+const popupFieldDescription = popup.querySelector('.popup__field_description');
+const popupCloseButton = popup.querySelector('.popup__close-button');
+
+const popupToggle = function() {
+    popup.classList.toggle('popup_opened');
+  }
+  
+  profileEditButton.addEventListener('click', popupToggle);
+  popupCloseButton.addEventListener('click', popupToggle);
+  
+  function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = popupFieldName.value;
+    profileDescription.textContent = popupFieldDescription.value;
+    popupToggle();
+  }
+  
+  formElement.addEventListener('submit', formSubmitHandler);
+  

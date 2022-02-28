b0eab6c9fe09f28fb9f8c51f5dae9a6916aaf765
Nataly Sagalaeva <garna@yandex.ru>
2022-02-28 12:39:54 +0300

    feat:  правки3

--- scripts/index.js
+++ scripts/index.js
@@ -3,25 +3,40 @@ const profileEditButton = document.querySelector('.profile__edit-button');
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 const formElement = document.querySelector('.popup__form');
-const popupFieldName = popup.querySelector('.popup__field_name');
-const popupFieldDescription = popup.querySelector('.popup__field_description');
+const popupName = popup.querySelector('.popup__name');
+const popupDescription = popup.querySelector('.popup__description');
 const popupCloseButton = popup.querySelector('.popup__close-button');
-const popupSaveButton = popup.querySelector('.popup__save-button');
 
-function popupToggle () {
-  popup.classList.toggle('popup__opened');
+/* Открытие поп апа */
+function popupOpened(popup) {
+  popup.classList.add('popup_opened');
 }
-  
-profileEditButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
 
-function formSubmitHandler (evt) {
+/* Закрытие поп апа */
+function popupClose(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+// Обработчик «отправки» формы
+function formSubmitHandler(evt) {
   evt.preventDefault();
-  profileTitle.textContent = popupFieldName.value;
-  profileDescription.textContent = popupFieldDescription.value;
-  popupToggle();
+
+   // Получение значение полей jobInput и nameInput из свойства value
+  profileTitle.textContent = popupName.value;
+  profileDescription.textContent = popupDescription.value;
+
+  popupClose(popup); //В функцию popupClose() передаём аргумент popup переменной элемента, с которой нужно удалить класс.
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
 
 
+
+profileEditButton.addEventListener('click', () => popupOpened(popup)); 
+//В момент клика по profileEditButton, вызываем колбэк функцию popupOpened() 
+//в которой передаём аргумент с переменной, на которой нужно навесить класс открытия, это переменная сейчас const popup.
+//Это стрелочная функция кстати. Вы должны были уже её пройти по идеи.
+
+popupCloseButton.addEventListener('click', () => popupClose(popup)); 
+//В момент клика по popupCloseButtone, вызываем колбэк функцию popupClose() 
+//в которой передаём аргумент с переменной, на которой нужно удалить класс открытия, это переменная сейчас const popup.
